9d5216b6ad0b7df59955d81fda92d3e2ad5e1039
shraddhabidaye <shraddhabidaye@gmail.com>
2021-07-14 17:31:26 +0530

    updated

--- assignment_1/4.php
+++ assignment_1/4.php
@@ -1,9 +1,10 @@
 <?php
-  echo"the key is:4"."<br>";
+  $key=4;
+  echo"the key is:$key"."<br>";
   $arr=array(1,4,8,6,9,7);
   echo"the array is:";
   print_r($arr);
-  if(in_array(4,$arr))
+  if(in_array($key,$arr))
     {
       echo"<br> Key found";
     }
