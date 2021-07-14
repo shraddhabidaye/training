28513f093f26cc1dcedd3e8157fa9f4ba3a3e81a
shraddhabidaye <shraddhabidaye@gmail.com>
2021-07-14 11:00:30 +0530

    solved Q1

--- assignment_1/1.php
+++ assignment_1/1.php
@@ -1,14 +1,20 @@
 <?php
-  $currentyear=idate("Y");
-  for($i=$currentyear;$i<=$currentyear+4;$i++)
+  abstract class Fruit                                  //parent class
     {
-      $count=0;
-      if($i%400==0 || $i%4==0)
-        {
-          echo"The next leap year is:",$i;
-          $count++;
-        }
-      if($count>0)
-        break;
+      public $name;
+      public function __construct($name)
+      {
+        $this->name = $name;
+      }
+      abstract public function getFruit() : string;
     }
+  class Fruitname extends Fruit                          //child class
+    {
+      public function getFruit() : string               //abstract function definition
+      {
+        return "this is $this->name";
+      }
+    }
+    $apple=new Fruitname("apple");
+    echo $apple->getFruit();
 ?>
