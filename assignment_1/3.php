3e9265712e998e782ff0167d70d4e63fbdbf0756
shraddhabidaye <shraddhabidaye@gmail.com>
2021-07-14 17:31:14 +0530

    updated

--- assignment_1/3.php
+++ assignment_1/3.php
@@ -1,7 +1,39 @@
 <?php
-  echo "the string1 is:This is Cat <br> the string2 is:this is cat<br>";
-  echo "output of string compare without case sensitive:";
-  echo strncasecmp("This is Cat","this is cat",9) . "<br>";
-  echo "output of string compare with case sensitive:";
-  echo strncmp("This is Cat","this is cat",9);
+  $str1="This is Cat";
+  $str2="this is cat";
+  $str3="hello world";
+  echo "the string1:$str1 <br> ";
+  echo"the string2 is:$str2 <br> ";
+  echo"the string3 is:$str3 <br>";
+  echo "comparing string1 & string2 <br>";
+  $result=strncasecmp($str1,$str2,9);
+
+  if ($result==0)
+    {
+      echo"string matched <br>";
+    }
+  else
+   {
+      echo " string is different <br>";
+    }
+  echo "comparing string1 & string2(case sensitive) <br>";
+  $result=strncmp($str1,$str2,9);
+  if ($result==0)
+    {
+      echo"string matched <br>";
+    }
+  else
+    {
+      echo " string is different <br>";
+    }
+    echo "comparing string1 & string3 <br>";
+    $result=strcmp($str1,$str3,);
+    if ($result==0)
+      {
+        echo"string matched <br>";
+      }
+    else
+      {
+        echo " string is different <br>";
+      }
 ?>
