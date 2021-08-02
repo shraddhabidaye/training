ad6cf80e21108866d3ecac6957f4c00721b0bc3b
shraddhabidaye <shraddhabidaye@gmail.com>
2021-08-02 16:48:35 +0530

    db connection

---
+++ php-mysql_assignment/config.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$dbHost = 'localhost';
+$dbName = 'registration';
+$dbUsername = 'root';
+$dbPassword = 'password';
+$dbc= mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+ ?>
