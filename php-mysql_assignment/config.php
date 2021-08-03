d05f594c19a79be918c07721143c5aad06ffc46e
shraddhabidaye <shraddhabidaye@gmail.com>
2021-08-03 21:52:51 +0530

    css style added

--- php-mysql_assignment/config.php
+++ php-mysql_assignment/config.php
@@ -1,53 +1,20 @@
 <?php
   session_start();
-  $dbHost = 'localhost';
-  $dbName = 'registration';
-  $dbUsername = 'root';
-  $dbPassword = '';
-  $dbc= mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-  
-if (isset($_POST['reg_user'])) {
+  $servername = "localhost";
+  $username = "root";
+  $password = "password";
+  $db = "registration";
 
-  // receive all input values from the form
-  $firstname = mysqli_real_escape_string($db, $_POST['name1']);
-  $lastname = mysqli_real_escape_string($db, $_POST['name2']);
-  $contact = mysqli_real_escape_string($db, $_POST['num1']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['psw']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['psw-repeat']);
+// Create connection
+ $conn = new mysqli($servername, $username, $password, $db);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-  if (empty($firstname)) { array_push($errors, "firstname is required"); }
-  if (empty($lastname)) { array_push($errors, "lastname is required"); }
-  if (empty($contact)) { array_push($errors, "contact no. is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+// Check connection
+ if ($conn->connect_error)
+  {
+   die("Connection failed: " . $conn->connect_error);
   }
-
-  // first check the database to make sure
-  // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-
-  if ($user) { // if user exists
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
-    }
-  }
-
-  // Finally, register user if there are no errors in the form
-//  if (count($errors) == 0) {
-
-  	$query = "INSERT INTO users (id,first_name,last_name,email,mobile_no,password)
-  			  VALUES('', '$firstname', '$lastname', '$email', '$contact', '$password_1')";
-  	mysqli_query($db, $query);
-  	$_SESSION['email'] = $email;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
-//  }
-}
- ?>
+ else
+ {
+ //echo "Connected successfully";
+ }
+?>
--- php-mysql_assignment/index.php
+++ php-mysql_assignment/index.php
@@ -1,19 +1,17 @@
-<?php require_once("config.php") ?>
+<?php
+  require_once("config.php");
+?>
 <html>
-    <head>
-      <title>
-        user login
-      </title>
+  <head>
+    <title>user login </title>
       <link rel="stylesheet" href="login.css">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    </head>
-    <body>
-     <form  style="max-width:500px;margin:auto">
-      <h2>Login Form</h2>
-
+  </head>
+  <body>
+    <form method="post" action="login_backend.php" style="max-width:500px;margin:auto">
+      <center><h2>Login Form</h2></center>
 
       <div class="input-container">
         <i class="fa fa-envelope icon"></i>
@@ -25,12 +23,14 @@
         <input class="input-field" type="password" placeholder="Password" name="psw">
       </div>
 
-      <button type="submit" class="btn">Login</button>
-      <div class="container signin">
-       <p> Don't have an account?</p>
-       <button  style="width:auto;"><a href="register.html">Sign Up</a></button>
+      <button type="submit" class="btn" name="login_btn">Login</button>
 
+      <div class="container-signin">
+        <p> Don't have an account?</p>
       </div>
+
+      <button class="btn"><a href="register.php" style="color:white">Sign Up</a></button>
+
     </form>
   </body>
 </html>
