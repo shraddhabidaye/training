a6cfad554b84474fa4b9f2a92d1e8d1a59148f01
shraddhabidaye <shraddhabidaye@gmail.com>
2021-08-03 21:58:43 +0530

    css style added

--- php-mysql_assignment/register.php
+++ php-mysql_assignment/register.php
@@ -1,19 +1,18 @@
-<?php  require_once("config.php"); ?>
-<html lang="en">
+<?php
+  require_once("config.php");
+?>
+<html>
   <head>
-
     <title>register</title>
     <link rel="stylesheet" href="login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
   </head>
   <body>
-    <form>
-
-      <div>
-        <form>
-          <div class="container">
+    <div>
+      <form method="post" action="register_backend.php">
+        <div class="container">
+          <div class="container1">
             <h1>Sign Up</h1>
             <p>Please fill in this form to create an account.</p>
             <hr>
@@ -38,12 +37,9 @@
             <div class="clearfix">
               <button type="submit" class="signupbtn" name="reg_user">Sign Up</button>
             </div>
-          </div>
-        </form>
-
+          </div>  
+        </div>
+      </form>
+    </div>
   </body>
 </html>
-
-<?php
-
- ?>
