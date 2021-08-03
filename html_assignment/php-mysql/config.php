3d1b6fbb6bfb1945baa380e8d815a2c2cfc8dce3
shraddhabidaye <shraddhabidaye@gmail.com>
2021-08-03 10:01:09 +0530

    db connection file

---
+++ html_assignment/php-mysql/config.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$db = "registration";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}else{
+ //echo "Connected successfully";
+}
+?>
