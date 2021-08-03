d69adb1bfebed6278d50a91146af6bbbd8f47666
shraddhabidaye <shraddhabidaye@gmail.com>
2021-08-03 21:33:09 +0530

    added css

---
+++ php-mysql/config.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+  $dbHost = 'localhost';
+  $dbName = 'registration';
+  $dbUsername = 'root';
+  $dbPassword = '';
+  $dbc= mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+echo "string";
+
+ ?>
