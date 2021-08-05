e6b8a4a48a5b8aab4807bce90dfbeacc00ba22e5
shraddhabidaye <shraddhabidaye@gmail.com>
2021-08-05 21:55:38 +0530

    updated the variables

--- ajax/register.php
+++ ajax/register.php
@@ -1,101 +1,79 @@
-
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>register</title>
-    <link rel="stylesheet" href="login.css">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-  </head>
-  <body>
-
+<head>
+	<title>Insert data in MySQL database using Ajax</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+<div style="margin: auto;width: 60%;">
 	<div class="alert alert-success alert-dismissible" id="success" style="display:none;">
 	  <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
 	</div>
-    <div>
-      <form method="post" id="form">
-        <div class="container">
-          <div class="container1">
-            <h1>Sign Up</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr>
-            <label for="first_name"><b>First Name:</b></label>
-            <input type="text" placeholder="first name" id="name1" required>
+	<form id="fupForm" name="form1" method="post">
+		<div class="form-group">
+			<label for="name">Name:</label>
+			<input type="text" class="form-control" id="fname" placeholder="Name" name="name1">
+		</div>
+    <div class="form-group">
+			<label for="name"> L Name:</label>
+			<input type="text" class="form-control" id="lname" placeholder="Name" name="name2">
+		</div>
+		<div class="form-group">
+			<label for="email">Email:</label>
+			<input type="email" class="form-control" id="email" placeholder="Email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="contact">Phone:</label>
+			<input type="text" class="form-control" id="contact" placeholder="Phone" name="contact">
+		</div>
+		<div class="form-group" >
+			<label for="pwd">password:</label>
+      <input type="password" class="form-control" id="pass1" placeholder="Phone" name="pass1">
+		</div>
+    <div class="form-group" >
+			<label for="pwd">password again:</label>
+      <input type="password" class="form-control" id="pass2" placeholder="Phone" name="pass2">
+		</div>
+		<input type="button" name="save" class="btn btn-primary" value="Save to database" id="butsave">
+	</form>
+</div>
 
-            <label for="last_name"><b>Last Name:</b></label>
-            <input type="text" placeholder="last name" id="name2" required>
+<script>
+$(document).ready(function() {
+	$('#butsave').on('click', function() {
+		$("#butsave").attr("disabled", "disabled");
+		var fname = $('#name1').val();
+    	var lname = $('#name2').val();
+      	var contact = $('#contact').val();
+        	var pass1 = $('#pass1').val();
+		var email = $('#email').val();
 
-            <label for="mobile"><b>Contact No.:</b></label>
-            <input type="number" placeholder="contact no." id="num1" required>
-
-            <label for="email"><b>Email</b></label>
-            <input type="email" placeholder="Enter Email" id="email" required>
-
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" id="psw" required>
-
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" id="psw-repeat" required>
-
-            <div class="clearfix">
-              <button type="submit" class="signupbtn" id="reg_user">Sign Up</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-
-    <script>
-      $(document).ready(function() {
-      	$('#reg_user').on('click', function() {
-        		$("#reg_user").attr("disabled", "disabled");
-        		var fname = $('#name1').val();
-          	var lname = $('#name2').val();
-            var contact = $('#num1').val();
-        		var email = $('#email').val();
-        		var pass1 = $('#psw').val();
-            var pass2 = $('#psw-repeat').val();
-        		if(fname!="" && lname!="" && contact!="" && email!="" && pass1!="" && pass2!="")
-            {
-        			$.ajax({
-        				url: "save.php",
-        				type: "POST",
-        				data: {
-        					fname: fname,
-                  lname: lname,
-                  contact: contact,
-        					email: email,
-        					pass1: pass1,
-                  pass2: pass2
-        				},
-        				cache: false,
-        				success: function(dataResult)
-                {
-        					var dataResult = JSON.parse(dataResult);
-        					if(dataResult.statusCode==200)
-                  {
-        						$("#reg_user").removeAttr("disabled");
-        						$('#form').find('input:text').val('');
-        						$("#success").show();
-        						$('#success').html('Data added successfully !');
-        					}
-        					else if(dataResult.statusCode==201)
-                  {
-        					   alert("Error occured !");
-        					}
-
-        				}
-        			});
-        		}
-        		else
-            {
-        			alert('Please fill all the field !');
-        		}
-        });
-  });
-
-    </script>
-  </body>
+		if(fname!="" && lname!="" && contact!="" && pass1!="" && email!=""){
+			$.ajax({
+				url: "save.php",
+				type: "POST",
+				data: {
+					fname: fname,
+          lname: lname,
+          contact: contact,
+          pass1: pass1,
+					email: email,
+				},
+        async: true,
+        contentType: false,
+        processData: false,
+				cache: false,
+				success: function(returndata){
+          alert(returndata);
+				}
+			});
+		}
+		else{
+			alert('Please fill all the field !');
+		}
+	});
+});
+</script>
+</body>
 </html>
--- ajax/save.php
+++ ajax/save.php
@@ -1,19 +1,20 @@
 <?php
-  include("config.php");
-
-  $fname=$_POST['fname'];
+	include 'config.php';
+	$fname=$_POST['fname'];
   $lname=$_POST['lname'];
-  $contact=$_POST['contact'];
 	$email=$_POST['email'];
+	$contact=$_POST['contact'];
 	$password=$_POST['pass1'];
-  $query1 = "INSERT INTO users (first_name,last_name,email,mobile_no,password)
-   VALUES('".$fname."', '".$lname."', '".$email."', ".$contact.", '".$password."')";
-$res = $conn->query($query1);
-	if (mysqli_query($conn, $query1)) {
+  echo $fname;
+	$sql = "INSERT INTO users (first_name,last_name,email,mobile_no,password)
+	VALUES ('$fname','$lname','$email','$contact','$password')";
+  echo $sql;
+$conn->query($sql);
+	if (mysqli_query($conn, $sql)) {
 		echo json_encode(array("statusCode"=>200));
 	}
 	else {
 		echo json_encode(array("statusCode"=>201));
 	}
 	mysqli_close($conn);
- ?>
+?>
