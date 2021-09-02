9686bcbc0e43f6e610c6e0552400ffaa26b9c82c
shraddhabidaye <shraddhabidaye@gmail.com>
2021-09-02 21:50:25 +0530

    created custom api to show available blog list

---
+++ custom_api/demo_restapi/src/Plugin/rest/resource/DemoResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\demo_restapi\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "demo_resource",
+ *   label = @Translation("Demo Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/demo_restapi/demo_resource"
+ *   }
+ * )
+ */
+
+class DemoResource extends ResourceBase
+{
+
+  /**
+   * Responds to entity GET requests.
+   * @return \Drupal\rest\ResourceResponse
+   */
+  public function get() {
+    $response = ['message' => 'Hello, this is a rest service'];
+    return new ResourceResponse($response);
+  }
+}
---
+++ custom_api/demo_restapi/src/Plugin/rest/resource/ListResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\demo_restapi\Plugin\rest\resource;
+
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "list_resource",
+ *   label = @Translation("list rest resource"),
+ *   uri_paths = {
+ *     "canonical" = "/demo_restapi/list_resource"
+ *   }
+ * )
+ */
+
+ class ListResource extends ResourceBase
+ {
+   /**
+  * A current user instance.
+  *
+  * @var \Drupal\Core\Session\AccountProxyInterface
+  */
+   protected $currentUser;
+
+   /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+   public function __construct(
+   array $configuration,
+   $plugin_id,
+   $plugin_definition,
+   array $serializer_formats,
+   LoggerInterface $logger,
+   AccountProxyInterface $current_user)
+    {
+       parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+       $this->currentUser = $current_user;
+    }
+    /**
+  * {@inheritdoc}
+  */
+   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+   {
+     return new static(
+       $configuration,
+       $plugin_id,
+       $plugin_definition,
+       $container->getParameter('serializer.formats'),
+       $container->get('logger.factory')->get('demo_restapi'),
+       $container->get('current_user')
+     );
+   }
+
+   /**
+   * Responds to GET requests.
+   *
+   * Returns a list of bundles for specified entity.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws exception expected.
+   */
+   public function get()
+   {
+     // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+    $entities = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadByProperties(['type' => 'blog']);
+    foreach ($entities as $entity) {
+      $result[$entity->id()] = $entity->title->value;
+    }
+
+    $response = new ResourceResponse($result);
+    $response->addCacheableDependency($result);
+    return $response;
+   }
+ }
