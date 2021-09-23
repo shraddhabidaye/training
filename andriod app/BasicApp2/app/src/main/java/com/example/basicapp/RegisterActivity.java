aa2a80a1edda71ee85cb207b0a549a374f5e97c6
shraddhabidaye <shraddhabidaye@gmail.com>
2021-09-23 18:26:56 +0530

    added the select imagefield

--- andriod app/BasicApp2/app/src/main/java/com/example/basicapp/RegisterActivity.java
+++ andriod app/BasicApp2/app/src/main/java/com/example/basicapp/RegisterActivity.java
@@ -1,35 +1,77 @@
 package com.example.basicapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String category,image;
+    ImageView imageView;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Spinner spinner = findViewById(R.id.spinner1);
+        spinner = findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.category, android.R.layout.simple_spinner_item);
+                R.array.blogCategory, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
     }
 
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String text = parent.getItemAtPosition(position).toString();
-
+        if(text.equals("Study")){
+            category = "4";
+        }
+        if(text.equals("Travel")){
+            category = "3";
+        }
+        if(text.equals("Political")){
+            category = "2";
+        }if(text.equals("Food")){
+            category = "1";
+        }
     }
 
     @Override
@@ -37,21 +79,143 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
 
     }
 
+    public void onSelectClick(View v)
+    {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+        {
+            // when permission is nor granted
+            // request permission
+            ActivityCompat.requestPermissions(RegisterActivity.this
+                    , new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+
+        }
+        else
+        {
+            // when permission
+            // is granted
+            // create method
+            selectImage();
+        }
+    }
+
+    public void selectImage()
+    {
+        imageView.setImageBitmap(null);
+        // Initialize intent
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // set type
+        intent.setType("image/*");
+        // start activity result
+        startActivityForResult(Intent.createChooser(intent,"Select Image"),100);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // check condition
+        if (requestCode==100 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            // when permission
+            // is granted
+            // call method
+            selectImage();
+        }
+        else
+        {
+            // when permission is denied
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check condition
+        if (requestCode==100 && resultCode==RESULT_OK && data!=null)
+        {
+            // when result is ok
+            // initialize uri
+            Uri uri=data.getData();
+            // Initialize bitmap
+            try {
+                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                // initialize byte stream
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                // compress Bitmap
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+                // Initialize byte array
+                byte[] bytes=stream.toByteArray();
+                // get base64 encoded string
+                image= Base64.encodeToString(bytes,Base64.DEFAULT);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void onSaveClick(View v)
     {
-        EditText ed1,ed2,ed3,ed4,ed5;
+        EditText ed1,ed2,ed3,ed4;
         ed1 = (EditText)findViewById(R.id.editText1);
         ed2 = (EditText)findViewById(R.id.editText2);
         ed3 = (EditText)findViewById(R.id.editText3);
         ed4 = (EditText)findViewById(R.id.editText4);
-        ed5 = (EditText)findViewById(R.id.editText5);
 
         String title = ed1.getText().toString();
         String body = ed2.getText().toString();
         String email = ed3.getText().toString();
         String image_filename = ed4.getText().toString();
-        String image = ed5.getText().toString();
 
+
+        String url = "https://dev-team-shivaji.pantheonsite.io/api/create_blog?_format=json";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response)
+            {
+                Log.e("Rest Response", response.toString());
+                Toast.makeText(RegisterActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+
+            }
+        },new com.android.volley.Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Rest Response", error.toString());
+
+                Toast.makeText(RegisterActivity.this, "Failed to add data", Toast.LENGTH_SHORT).show();
+            }
+        })
+            {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    String credentials = "admin:admin";
+                    String auth = "Basic "
+                            + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", auth);
+                    return headers;
+                }
+
+                @Override
+                public Map<String, String > getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("title", title);
+                    params.put("body", body);
+                    params.put("email", email);
+                    params.put("category",category);
+                    params.put("image",image);
+                    params.put("image_filename", image_filename);
+
+                    return params;
+                }
+            };
     }
 
 
